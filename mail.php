0de1b1f47124a75f1b206a7551d51119d924fb2c
Iglesiagalatas <88063019+Iglesiagalatas@users.noreply.github.com>
2021-07-27 19:39:35 -0600

    Add files via upload

--- mail.php
+++ mail.php
@@ -1,16 +1,28 @@
 <?php
-if(isset( $_POST['name']))
-$name = $_POST['name'];
-if(isset( $_POST['email']))
-$email = $_POST['email'];
-if(isset( $_POST['message']))
-$message = $_POST['message'];
-if(isset( $_POST['subject']))
-$subject = $_POST['subject'];
+if(isset($_POST['submit'])){
 
-$content="From: $name \n Email: $email \n Message: $message";
-$recipient = "paginagalatas@gmail.com";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $content, $mailheader) or die("Error!");
-echo "Email sent!";
+      $nombre = htmlspecialchars($_POST['name']);
+      $contenido = htmlspecialchars($_POST['message']);
+      $subject = htmlspecialchars($_POST['subject']);
+      $para = "paginagalatas@gmail.com";
+      $de = htmlspecialchars($_POST['email']);
+      $mensaje = "Mensaje de la Pagina web";
+      $mensaje .= "\n";
+      $mensaje .= "Nombre: ".$nombre."\n";
+      $mensaje .= "Correo: ".$de."\n";
+      $mensaje .= "Comentarios: ".$contenido."\n\n";
+      $mensaje .= "----------------------\n";
+      
+      if(mail($para, $subject,$mensaje,"From: $de")){
+          header("Location: index.html");
+          exit();
+      }
+      
+      // fin
+  
+  } else {
+  
+      echo "No se ha enviado ningún correo";
+  
+  }
 ?>
